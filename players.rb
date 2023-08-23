42cd4e4407ae7653d345f2c7cd42924ba9717606
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-08-22 23:55:54 -0400

    attr_accessor for player-number

--- players.rb
+++ players.rb
@@ -6,11 +6,12 @@
   # public methods - getter and setter for score and life remaining
 
 class Players
-  attr_accessor :life_remaining, :score
+  attr_accessor :life_remaining, :score, :player_number
 
-  def initialize(life_remaining, score)
+  def initialize(life_remaining, score, player_number)
     @life_remaining = life_remaining
     @score = score
+    @player_number = player_number
   end
 
 end
