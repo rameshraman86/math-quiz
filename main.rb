293bcbf2d513c92bf32c0655bf40cce46b054aca
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-08-22 23:55:30 -0400

    game logic

--- main.rb
+++ main.rb
@@ -18,15 +18,18 @@
 require_relative './players'
 require_relative './game'
 
-player1 = Players.new(3, 0)
-player2 = Players.new(3, 0)
+player1 = Players.new(3, 0, 1)
+player2 = Players.new(3, 0, 2)
 
 game = Game.new()
 game.add_players(player1)
 game.add_players(player2)
 
+game.init_game
+
 while game.winner == nil
   puts "----- NEW TURN -----"
+  game.change_question
   puts "Player #{game.current_player.player_number}: #{game.curr_question[:question]}"
   answer = gets.chomp.to_i
   if answer == game.curr_question[:answer]
@@ -36,10 +39,12 @@ while game.winner == nil
     puts "Seriously? No!"
     game.reduce_player_life(game.current_player)
   end
-  game.change_player
-  game.change_question
-  game.check_winner
+
   puts "P1: #{player1.score}/3 vs P2: #{player2.score}/3"
+  game.change_player
+  if(game.check_winner)
+    game.display_winner
+  end
 end
 
 
