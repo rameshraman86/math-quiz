d51c87d68b10a815083ddd649c5273178ab2dee5
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-08-22 22:27:27 -0400

    initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+# player
+  # in order to be initiatlized, they will need
+    # player number = 1 or 2
+    # life_remaining
+    # score
+  # public methods - getter and setter for score and life remaining
+# game
+  # questions = { question: '...', answer : '..' }
+  # players 
+  # curr_question
+  # current_player 
+  # winner 
+    # METHODS
+      # add 2 players - create 2 player instances?
+      # increase_player_score by 1
+      # reduce_player_life by 1
+  
